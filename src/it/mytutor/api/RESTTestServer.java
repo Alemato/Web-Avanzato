7f0d00c2027897695f2c7f6109a9a82e57588915
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-05-22 10:18:59 +0200

    prima get per testare il server se funziona

---
+++ src/it/mytutor/api/RESTTestServer.java
@@ -0,0 +1,17 @@
+package it.mytutor.api;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("test")
+public class RESTTestServer {
+    @GET
+    @Consumes("application/jason")
+    @Produces("text/plani")
+    public String testserverstring(){
+        return "FUNZIONA" ;
+    }
+
+}
