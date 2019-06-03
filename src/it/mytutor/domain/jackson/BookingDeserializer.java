7bb99cfb9bf5b1edd7f272a6190a7bdb48283a99
Stefano Decina <stefano.decina@student.univaq.it>
2019-06-03 19:06:29 +0200

    Deserializzatore Booking

--- src/it/mytutor/domain/jackson/BookingDeserializer.java
+++ src/it/mytutor/domain/jackson/BookingDeserializer.java
@@ -6,10 +6,14 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import it.mytutor.domain.Booking;
+import it.mytutor.domain.Lesson;
+import it.mytutor.domain.Student;
 
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingDeserializer extends StdDeserializer<Booking> {
 
@@ -22,17 +26,31 @@ public class BookingDeserializer extends StdDeserializer<Booking> {
     }
 
     @Override
-    public Booking deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+    public Booking deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
 
         Booking booking = new Booking();
-//        booking.setIdBooking(node.get("idBooking").asInt());                       //TODO da capire se arriva già con id
-//        booking.setDate(Date.valueOf(node.get("date").asText()));
-//        booking.setLessonState(node.get("lessonState").asBoolean());
+
+        booking.setIdBooking(node.get("idBooking").asInt());                       //TODO da capire se arriva già con id
+
+        booking.setDate(Date.valueOf(node.get("date").asText()));
+
+        booking.setLessonState(node.get("lessonState").asInt());
+
 //        booking.setCreateDate(Timestamp.valueOf(node.get("createDate").asText()));  //TODO da capire se arriva già con createDate
 //        booking.setUpdateDate(Timestamp.valueOf(node.get("updateDate").asText()));  //TODO da capire se arriva già con UpdateDate
-//        booking.setIdStudent(node.get("idStudent").asInt());                       //TODO da capire se arriva già con id
-//        booking.setIdLesson(node.get("idLesson").asInt());
+
+        //TODO da capire se basta l'id della lezione
+        Student student = new Student();
+        student.setIdStudent(node.findPath("student").findPath("idStudent").asInt());
+        booking.setStudent(student);
+
+        //TODO da capire se basta l'id della lezione
+        Lesson lesson = new Lesson();
+        lesson.setIdLesson(node.findPath("lesson").findPath("idLesson").asInt());
+//
+        booking.setLesson(lesson);
+
         return booking;
     }
 }
