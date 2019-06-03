77d9332885ed9bf4e3f829f7c3f4e270427e2221
Stefano Decina <stefano.decina@student.univaq.it>
2019-06-03 19:08:47 +0200

    Serializzatore Booking

--- src/it/mytutor/domain/jackson/BookingSerializer.java
+++ src/it/mytutor/domain/jackson/BookingSerializer.java
@@ -20,14 +20,19 @@ public class BookingSerializer extends StdSerializer<Booking> {
     @Override
     public void serialize(Booking booking, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
 
+        jsonGenerator.writeStartObject("booking");
+        jsonGenerator.writeNumberField("idBooking", booking.getIdBooking());
+        jsonGenerator.writeStringField("date", booking.getDate().toString());
+        jsonGenerator.writeNumberField("lessonState", booking.getLessonState());
+        jsonGenerator.writeStringField("createDate", booking.getCreateDate().toString());
+        jsonGenerator.writeStringField("updateDate", booking.getUpdateDate().toString());
         jsonGenerator.writeStartObject();
-//        jsonGenerator.writeNumberField("idBooking", booking.getIdBooking());
-//        jsonGenerator.writeStringField("date", booking.getDate().toString());
-//        jsonGenerator.writeBooleanField("lessonState", booking.isLessonState());
-//        jsonGenerator.writeStringField("createDate", booking.getCreateDate().toString());
-//        jsonGenerator.writeStringField("updateDate", booking.getUpdateDate().toString());
-//        jsonGenerator.writeNumberField("idStudent", booking.getIdStudent());
-//        jsonGenerator.writeNumberField("idLesson", booking.getIdLesson());
+        jsonGenerator.writeObjectField("student", booking.getStudent());
         jsonGenerator.writeEndObject();
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeObjectField("lesson", booking.getLesson());
+        jsonGenerator.writeEndObject();
+        jsonGenerator.writeEndObject();
+        jsonGenerator.close();
     }
 }
