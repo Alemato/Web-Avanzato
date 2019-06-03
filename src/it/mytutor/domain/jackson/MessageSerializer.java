2ecfc8ad038dbbe5f19fa103dee2c1c967555913
Stefano Decina <stefano.decina@student.univaq.it>
2019-06-03 19:09:23 +0200

    Serializzatore Message

--- src/it/mytutor/domain/jackson/MessageSerializer.java
+++ src/it/mytutor/domain/jackson/MessageSerializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import it.mytutor.domain.Message;
+import it.mytutor.domain.User;
 
 import java.io.IOException;
 
@@ -19,14 +20,31 @@ public class MessageSerializer extends StdSerializer<Message> {
     @Override
     public void serialize(Message message, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
 
-        jsonGenerator.writeStartObject();
-//        jsonGenerator.writeNumberField("idMessage",message.getIdMessage());
-//        jsonGenerator.writeStringField("text", message.getText());
-//        jsonGenerator.writeStringField("sendDate",message.getSendDate().toString());
-//        jsonGenerator.writeStringField("createDate",message.getCreateDate().toString());
-//        jsonGenerator.writeStringField("updateDate",message.getUpdateDate().toString());
-//        jsonGenerator.writeNumberField("idChat",message.getIdChat());
-//        jsonGenerator.writeNumberField("idUser",message.getIdUser());
+        jsonGenerator.writeStartObject("message");
+
+        jsonGenerator.writeNumberField("idMessage",message.getIdMessage());
+
+        jsonGenerator.writeStringField("text", message.getText());
+
+        jsonGenerator.writeStringField("sendDate",message.getSendDate().toString());
+
+        jsonGenerator.writeStringField("createDate",message.getCreateDate().toString());
+        jsonGenerator.writeStringField("updateDate",message.getUpdateDate().toString());
+
+        jsonGenerator.writeStartObject("chat");
+        jsonGenerator.writeObjectField("chat", message.getIdChat());
         jsonGenerator.writeEndObject();
+
+        jsonGenerator.writeFieldName("users");
+        jsonGenerator.writeStartArray();
+        for (User user: message.getIdUser()){
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeObjectField("user",user);
+            jsonGenerator.writeEndObject();
+        }
+        jsonGenerator.writeEndArray();
+
+        jsonGenerator.writeEndObject();
+        jsonGenerator.close();
     }
 }
