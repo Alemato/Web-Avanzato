64eb78da42c062273e07b3af789c508e35c9f2b4
Stefano Decina <stefano.decina@student.univaq.it>
2019-07-03 07:35:24 +0200

    TeacherSerializer debug

--- src/it/mytutor/domain/jackson/TeacherSerializer.java
+++ src/it/mytutor/domain/jackson/TeacherSerializer.java
@@ -1,6 +1,7 @@
 package it.mytutor.domain.jackson;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import it.mytutor.domain.Teacher;
@@ -13,26 +14,17 @@ public class TeacherSerializer extends StdSerializer<Teacher> {
     public TeacherSerializer(Class<Teacher> t) { super(t); }
 
     @Override
-    public void serialize(Teacher teacher, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        jsonGenerator.writeStartObject("teacher");
-
+    public void serialize(Teacher teacher, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeStartObject();
         jsonGenerator.writeNumberField("idTeacher", teacher.getIdTeacher());
-
         jsonGenerator.writeNumberField("postCode", teacher.getPostCode());
-
         jsonGenerator.writeStringField("City", teacher.getCity());
-
         jsonGenerator.writeStringField("region", teacher.getRegion());
-
         jsonGenerator.writeStringField("street", teacher.getStreet());
-
         jsonGenerator.writeStringField("streetNumber", teacher.getStreetNumber());
-
         jsonGenerator.writeStringField("byography", teacher.getByography());
-
         jsonGenerator.writeStringField("crateDateTeacher", teacher.getCrateDateTeacher().toString());
         jsonGenerator.writeStringField("updateDateTeacher",teacher.getUpdateDateTeacher().toString());
-
         jsonGenerator.writeFieldName("user");
         jsonGenerator.writeStartObject();
         jsonGenerator.writeNumberField("idUser", teacher.getIdUser());
@@ -45,7 +37,6 @@ public class TeacherSerializer extends StdSerializer<Teacher> {
         jsonGenerator.writeStringField("createDate", teacher.getCreateDate().toString());
         jsonGenerator.writeStringField("updateDate", teacher.getUpdateDate().toString());
         jsonGenerator.writeEndObject();
-
         jsonGenerator.writeEndObject();
     }
 }
