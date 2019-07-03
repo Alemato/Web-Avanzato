e73c6dc819903f8aa5135c7d8826a490d8a42182
Stefano Decina <stefano.decina@student.univaq.it>
2019-07-03 07:34:14 +0200

    PlanningSerializer add Teacher Object add lesson Object add subject Object debug

--- src/it/mytutor/domain/jackson/PlanningSerializer.java
+++ src/it/mytutor/domain/jackson/PlanningSerializer.java
@@ -1,6 +1,7 @@
 package it.mytutor.domain.jackson;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import it.mytutor.domain.Planning;
@@ -16,17 +17,45 @@ public class PlanningSerializer extends StdSerializer<Planning> {
     }
 
     @Override
-    public void serialize(Planning planning, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        jsonGenerator.writeStartObject("planning");
+    public void serialize(Planning planning, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeStartObject();
         jsonGenerator.writeNumberField("idPlanning", planning.getIdPlanning());
         jsonGenerator.writeStringField("date", planning.getDate().toString());
         jsonGenerator.writeStringField("startTime", planning.getStartTime().toString());
         jsonGenerator.writeStringField("endTime", planning.getEndTime().toString());
         jsonGenerator.writeStringField("createDate", planning.getCreateDate().toString());
         jsonGenerator.writeStringField("updateDate", planning.getUpdateDate().toString());
+        jsonGenerator.writeFieldName("teacher");
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeNumberField("idTeacher", planning.getTeacher().getIdTeacher());
 
-        jsonGenerator.writeStringField("idLesson", planning.getLesson().getIdLesson().toString());
-
-
+        jsonGenerator.writeStringField("region", planning.getTeacher().getRegion());
+        jsonGenerator.writeStringField("city", planning.getTeacher().getCity());
+        jsonGenerator.writeNumberField("postalCode", planning.getTeacher().getPostCode());
+        jsonGenerator.writeStringField("street", planning.getTeacher().getStreet());
+        jsonGenerator.writeStringField("streetNumber", planning.getTeacher().getStreetNumber());
+        jsonGenerator.writeStringField("teacherName", planning.getTeacher().getName());
+        jsonGenerator.writeStringField("teacherSurname", planning.getTeacher().getSurname());
+        jsonGenerator.writeEndObject();
+        jsonGenerator.writeFieldName("lesson");
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeNumberField("idLesson", planning.getLesson().getIdLesson());
+        jsonGenerator.writeStringField("name", planning.getLesson().getName());
+        jsonGenerator.writeNumberField("price", planning.getLesson().getPrice());
+        jsonGenerator.writeStringField("description", planning.getLesson().getDescription());
+        jsonGenerator.writeStringField("publicationDate", planning.getLesson().getPublicationDate().toString());
+        jsonGenerator.writeStringField("lessonCreateDate", planning.getLesson().getCreateDate().toString());
+        jsonGenerator.writeStringField("lessonUpdateDate", planning.getLesson().getUpdateDate().toString());
+        jsonGenerator.writeFieldName("subject");
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeStringField("idSubject", planning.getLesson().getSubject().getIdSubject().toString());
+        jsonGenerator.writeStringField("macroSubject", planning.getLesson().getSubject().getMacroSubject());
+        jsonGenerator.writeStringField("microSubject", planning.getLesson().getSubject().getMicroSubject());
+        jsonGenerator.writeStringField("subjectCreateTime", planning.getLesson().getSubject().getCreateDate().toString());
+        jsonGenerator.writeStringField("subjectUpdateTime", planning.getLesson().getSubject().getUpdateDate().toString());
+        jsonGenerator.writeEndObject();
+        jsonGenerator.writeEndObject();
+        jsonGenerator.writeEndObject();
+        jsonGenerator.close();
     }
 }
