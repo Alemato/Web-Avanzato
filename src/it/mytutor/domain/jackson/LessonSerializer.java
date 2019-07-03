5f08d4b1e2cf5778af183adea370f7ff16d61edc
Stefano Decina <stefano.decina@student.univaq.it>
2019-07-03 07:31:06 +0200

    LessonSerializzer remove planning field debug

--- src/it/mytutor/domain/jackson/LessonSerializer.java
+++ src/it/mytutor/domain/jackson/LessonSerializer.java
@@ -1,6 +1,7 @@
 package it.mytutor.domain.jackson;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import it.mytutor.domain.Lesson;
@@ -8,10 +9,11 @@ import it.mytutor.domain.Lesson;
 import java.io.IOException;
 import java.sql.Time;
 import java.util.Date;
+import java.util.List;
 
 public class LessonSerializer extends StdSerializer<Lesson> {
 
-    public LessonSerializer(){
+    public LessonSerializer() {
         this(null);
     }
 
@@ -20,46 +22,17 @@ public class LessonSerializer extends StdSerializer<Lesson> {
     }
 
     @Override
-    public void serialize(Lesson lesson, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-
-        jsonGenerator.writeStartObject("lesson");
+    public void serialize(Lesson lesson, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeStartObject();
         jsonGenerator.writeNumberField("idLesson", lesson.getIdLesson());
-
         jsonGenerator.writeStringField("name", lesson.getName());
-
         jsonGenerator.writeNumberField("price", lesson.getPrice());
-
-        jsonGenerator.writeFieldName("dates");
-        jsonGenerator.writeStartArray();
-        for (Date date: lesson.getDate()){
-            jsonGenerator.writeString(date.toString());
-        }
-        jsonGenerator.writeEndArray();
-
         jsonGenerator.writeStringField("description", lesson.getDescription());
-
         jsonGenerator.writeStringField("publicationDate", lesson.getPublicationDate().toString());
-
-        jsonGenerator.writeFieldName("startTimes");
-        jsonGenerator.writeStartArray();
-        for (Time startTime: lesson.getStartTime()){
-            jsonGenerator.writeString(startTime.toString());
-        }
-        jsonGenerator.writeEndArray();
-
-
-        jsonGenerator.writeFieldName("endTimes");
-        jsonGenerator.writeStartArray();
-        for (Time endTime: lesson.getEndTime()){
-            jsonGenerator.writeString(endTime.toString());
-        }
-        jsonGenerator.writeEndArray();
-
-        jsonGenerator.writeStringField("createDate",lesson.getCreateDate().toString());
-        jsonGenerator.writeStringField("updateDate",lesson.getUpdateDate().toString());
-
+        jsonGenerator.writeStringField("createDate", lesson.getCreateDate().toString());
+        jsonGenerator.writeStringField("updateDate", lesson.getUpdateDate().toString());
         jsonGenerator.writeFieldName("subject");
-        jsonGenerator.writeStartObject("subject");
+        jsonGenerator.writeStartObject();
         jsonGenerator.writeStringField("idSubject", lesson.getSubject().getIdSubject().toString());
         jsonGenerator.writeStringField("macroSubject", lesson.getSubject().getMacroSubject());
         jsonGenerator.writeStringField("microSubject", lesson.getSubject().getMicroSubject());
@@ -67,8 +40,6 @@ public class LessonSerializer extends StdSerializer<Lesson> {
         jsonGenerator.writeStringField("updateTime", lesson.getSubject().getUpdateDate().toString());
         jsonGenerator.writeEndObject();
         jsonGenerator.writeEndObject();
-
         jsonGenerator.close();
     }
-
 }
