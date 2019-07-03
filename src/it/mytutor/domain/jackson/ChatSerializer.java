b770701db9cc55238c5198c22480c1f428d428eb
Stefano Decina <stefano.decina@student.univaq.it>
2019-07-03 07:29:39 +0200

    ChatSeriariler debug

--- src/it/mytutor/domain/jackson/ChatSerializer.java
+++ src/it/mytutor/domain/jackson/ChatSerializer.java
@@ -1,6 +1,7 @@
 package it.mytutor.domain.jackson;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import it.mytutor.domain.Chat;
@@ -9,7 +10,7 @@ import java.io.IOException;
 
 public class ChatSerializer extends StdSerializer<Chat> {
 
-    public ChatSerializer(){
+    public ChatSerializer() {
         this(null);
     }
 
@@ -18,9 +19,9 @@ public class ChatSerializer extends StdSerializer<Chat> {
     }
 
     @Override
-    public void serialize(Chat chat, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        jsonGenerator.writeStartObject("chat");
-        jsonGenerator.writeNumberField("idChat",chat.getIdChat());
+    public void serialize(Chat chat, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeNumberField("idChat", chat.getIdChat());
         jsonGenerator.writeStringField("name", chat.getName());
         jsonGenerator.writeStringField("createDate", chat.getCreateDate().toString());
         jsonGenerator.writeStringField("updateDate", chat.getUpdateDate().toString());
