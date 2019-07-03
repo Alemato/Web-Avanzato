68881ac4a4d40fdf7590ca6b36f2975b79e8bf52
Stefano Decina <stefano.decina@student.univaq.it>
2019-07-03 07:38:46 +0200

    Planning Model add Teacher

--- src/it/mytutor/domain/Planning.java
+++ src/it/mytutor/domain/Planning.java
@@ -1,8 +1,11 @@
 package it.mytutor.domain;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Objects;
 
 public class Planning {
     private Integer idPlanning;
@@ -11,6 +14,12 @@ public class Planning {
     private Time endTime;
     private Timestamp createDate;
     private Timestamp updateDate;
+
+
+
+
+
+    private Teacher teacher;
     private Lesson lesson;
 
     public Planning() { super(); }
@@ -22,13 +31,14 @@ public class Planning {
         this.endTime = endTime;
     }
 
-    public Planning(Integer idPlanning, Date date, Time startTime, Time endTime, Timestamp createDate, Timestamp updateDate, Lesson lesson) {
+    public Planning(Integer idPlanning, Date date, Time startTime, Time endTime, Timestamp createDate, Timestamp updateDate, Teacher teacher, Lesson lesson) {
         this.idPlanning = idPlanning;
         this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
         this.createDate = createDate;
         this.updateDate = updateDate;
+        this.teacher = teacher;
         this.lesson = lesson;
     }
 
@@ -58,6 +68,10 @@ public class Planning {
 
     public Timestamp getUpdateDate() {
         return updateDate;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
     }
 
     public Lesson getLesson() {
@@ -92,7 +106,45 @@ public class Planning {
         this.updateDate = updateDate;
     }
 
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
     public void setLesson(Lesson lesson) {
         this.lesson = lesson;
+    }
+
+    @Override
+    public String toString() {
+        return "Planning{" +
+                "idPlanning=" + idPlanning +
+                ", date=" + date +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
+                ", teacher=" + teacher +
+                ", lesson=" + lesson +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Planning planning = (Planning) o;
+        return Objects.equals(idPlanning, planning.idPlanning) &&
+                Objects.equals(date, planning.date) &&
+                Objects.equals(startTime, planning.startTime) &&
+                Objects.equals(endTime, planning.endTime) &&
+                Objects.equals(createDate, planning.createDate) &&
+                Objects.equals(updateDate, planning.updateDate) &&
+                Objects.equals(teacher, planning.teacher) &&
+                Objects.equals(lesson, planning.lesson);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPlanning, date, startTime, endTime, createDate, updateDate, teacher, lesson);
     }
 }
