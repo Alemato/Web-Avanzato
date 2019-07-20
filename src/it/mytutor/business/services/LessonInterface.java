3baea41962df84ab6b61c71c0223c092788cfbda
Stefano Decina <stefano.decina@student.univaq.it>
2019-07-20 12:17:29 +0200

    Debug Lesson Business e relativa interface

--- src/it/mytutor/business/services/LessonInterface.java
+++ src/it/mytutor/business/services/LessonInterface.java
@@ -10,7 +10,7 @@ public interface LessonInterface {
     List<Lesson> findAllLesson ();
     List<Lesson> findAllLessonByTeacher(Teacher teacher);
     List<Lesson> findAllLessonBySubject(Subject subject);
-    Lesson findLessonByFilter(String filter);
+    List<Lesson> findLessonByFilter(String filter);
     Lesson createLesson(Lesson lesson);
     Lesson updateLessson(Lesson lesson);
 }
