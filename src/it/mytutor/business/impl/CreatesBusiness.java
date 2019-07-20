c9f33730b31173b01fb70f1f50b4e5ba258e6f70
Stefano Decina <stefano.decina@student.univaq.it>
2019-07-20 11:42:23 +0200

    CreatesBusiness e relativa interfaccia

---
+++ src/it/mytutor/business/impl/CreatesBusiness.java
@@ -0,0 +1,12 @@
+package it.mytutor.business.impl;
+
+import it.mytutor.business.services.CreatesInterface;
+import it.mytutor.domain.Creates;
+
+public class CreatesBusiness implements CreatesInterface {
+    //TODO Costruttore Creates
+
+    @Override
+    public Creates createCreates(Creates creates){ return null; }
+
+}
---
+++ src/it/mytutor/business/services/CreatesInterface.java
@@ -0,0 +1,7 @@
+package it.mytutor.business.services;
+
+import it.mytutor.domain.Creates;
+
+public interface CreatesInterface {
+    Creates createCreates(Creates creates);
+}
