d1ddaa3d7c35e96b860cbd3b2e7bb4d4732868bb
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-11-26 13:37:43 +0100

    Revert ""inserito la query GET TEACHE BY USER ID""
    This reverts commit b754482d

--- src/it/mytutor/domain/dao/interfaces/TeacherDaoInterface.java
+++ src/it/mytutor/domain/dao/interfaces/TeacherDaoInterface.java
@@ -14,6 +14,7 @@ public interface TeacherDaoInterface {
      Teacher getTeacherByID(int id) throws DatabaseException;
   /*
      Teacher getTeacherByName(String name) throws DatabaseException;*/
+     Teacher getTeacherByUserID(int userId) throws DatabaseException;
 
      List<Teacher> getAllTeacher()throws DatabaseException;
 }
