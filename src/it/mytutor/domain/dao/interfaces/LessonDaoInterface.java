98001a51fb64cabb0f3aeaff6d4b6241881876e3
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-13 09:12:37 +0100

    Lesson dao interfaces Bozza

--- src/it/mytutor/domain/dao/interfaces/LessonDaoInterface.java
+++ src/it/mytutor/domain/dao/interfaces/LessonDaoInterface.java
@@ -1,6 +1,7 @@
 package it.mytutor.domain.dao.interfaces;
 
 import it.mytutor.domain.Lesson;
+import it.mytutor.domain.Teacher;
 import it.mytutor.domain.dao.exception.DatabaseException;
 
 
@@ -9,18 +10,18 @@ import java.util.List;
 
 public interface LessonDaoInterface {
 
-    List<Lesson> getAllLesson()throws DatabaseException;
+    List<Lesson> getAllLesson() throws DatabaseException;
 
-    List<Lesson> getLessonByName (String name) throws DatabaseException;
+    List<Lesson> getLessonByName(String name) throws DatabaseException;
 
     List<Lesson> getLessonsBySubject(String microSubject) throws DatabaseException;
+
+    List<Lesson> getLessonsByTeacher(Teacher teacher) throws DatabaseException;
 
     Lesson getLessonsByID(int id) throws DatabaseException;
 
     void modifyLesson(Lesson lesson) throws DatabaseException;
 
 
-    void createLesson(Lesson lesson)throws DatabaseException;
-
-
+    void createLesson(Lesson lesson) throws DatabaseException;
 }
