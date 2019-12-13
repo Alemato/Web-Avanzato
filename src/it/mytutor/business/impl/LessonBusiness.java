b235cb40ae86b82b0dbde97d4e88bcf3c6e62fd0
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-13 09:09:58 +0100

    Lesson Business Bozza

--- src/it/mytutor/business/impl/LessonBusiness.java
+++ src/it/mytutor/business/impl/LessonBusiness.java
@@ -2,8 +2,11 @@ package it.mytutor.business.impl;
 
 import it.mytutor.business.services.LessonInterface;
 import it.mytutor.domain.Lesson;
+import it.mytutor.domain.Planning;
 import it.mytutor.domain.Subject;
 import it.mytutor.domain.Teacher;
+import it.mytutor.domain.dao.exception.DatabaseException;
+import it.mytutor.domain.dao.implement.LessonDao;
 
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -30,12 +33,8 @@ public class LessonBusiness implements LessonInterface {
     @Override
     public List<Lesson> findAllLessonByTeacher(Teacher teacher) {
 
-        List<Lesson> lessons = null;
-        try {
-            lessons = new ArrayList<>(simulateFindAllLesson());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        List<Lesson> lessons = new ArrayList<>();
+
         return lessons;
     }
 
@@ -44,23 +43,60 @@ public class LessonBusiness implements LessonInterface {
         return null;
     }
 
-    @Override
-    public List<Lesson> findLessonByFilter(String macroMateria, String nome, String zona, String microMateria,
-                                           String giornoSettimana, String prezzo, String oraInizio, String oraFine) {
-//        TODO logica del filtro con tutti i campi
-        return null;
-    }
+//    @Override
+//    public List<Lesson> findLessonByFilter(String macroMateria, String nome, String zona, String microMateria,
+//                                           String giornoSettimana, String prezzo, String oraInizio, String oraFine) throws DatabaseException {
+//        LessonDao lessonDao = new LessonDao();
+//        List<Lesson> lessons = new ArrayList<>();
+//        int macroMateriaRelevant  = 0;
+//        if (macroMateria != null && !macroMateria.isEmpty()){
+//            macroMateriaRelevant = 1;
+//        }
+//        int nomeRelevant  = 0;
+//        if (nome != null && !nome.isEmpty()){
+//            nomeRelevant = 1;
+//        }
+//        int zonaRelevant  = 0;
+//        if (zona != null && !zona.isEmpty()){
+//            zonaRelevant = 1;
+//        }
+//        int microMateriaRelevant  = 0;
+//        if (microMateria != null && !microMateria.isEmpty()){
+//            microMateriaRelevant = 1;
+//        }
+//        int giornoSettimanaRelevant  = 0;
+//        if (giornoSettimana != null && !giornoSettimana.isEmpty()){
+//            giornoSettimanaRelevant = 1;
+//        }
+//        int prezzoRelevant  = 0;
+//        if (prezzo != null && !prezzo.isEmpty()){
+//            prezzoRelevant = 1;
+//        }
+//        int oraInizioRelevant  = 0;
+//        if (oraInizio != null && !oraInizio.isEmpty()){
+//            oraInizioRelevant = 1;
+//        }
+//        int oraFineaRelevant  = 0;
+//        if (oraFine != null && !oraFine.isEmpty()){
+//            oraFineaRelevant = 1;
+//        }
+//
+//        lessons = lessonDao.getLessonByFilter(macroMateriaRelevant, macroMateria, nomeRelevant, nome,
+//                zonaRelevant, zona, microMateriaRelevant, microMateria, giornoSettimanaRelevant, giornoSettimana,
+//                prezzoRelevant, prezzo, oraInizioRelevant, oraInizio, oraFineaRelevant, oraFine);
+//        return lessons;
+//    }
     @Override
     public Lesson findLessonByID(Integer idLesson){
         return null;
     }
     @Override
-    public Lesson createLesson(Lesson lesson) {
-        return lesson;
+    public Planning createLesson(Planning planning) {
+        return planning;
     }
 
     @Override
-    public Lesson updateLessson(Lesson lesson) {
+    public Planning updateLessson(Planning planning) {
         return null;
     }
 }
