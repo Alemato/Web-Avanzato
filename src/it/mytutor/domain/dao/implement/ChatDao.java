f26e3c6caed2220e6be364a4b6d19d0e16e631f1
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-13 09:11:46 +0100

    Chat dao Bozza

---
+++ src/it/mytutor/domain/dao/implement/ChatDao.java
@@ -0,0 +1,27 @@
+package it.mytutor.domain.dao.implement;
+
+import it.mytutor.domain.Booking;
+
+import it.mytutor.domain.Lesson;
+import it.mytutor.domain.Student;
+import it.mytutor.domain.Teacher;
+import it.mytutor.domain.dao.daofactory.DaoFactory;
+import it.mytutor.domain.dao.exception.DatabaseException;
+import it.mytutor.domain.dao.interfaces.BookingDaoInterface;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChatDao {
+//    private static final String
+//    private static final String
+//    private static final String
+//    private static final String
+
+    private void configureChat(Booking booking, ResultSet resultSet) throws DatabaseException {
+
+    }
+}
