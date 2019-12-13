13ade460de16723f2ec3f187fd969b68bb751a1d
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-13 09:10:59 +0100

    Interfaces Lesson Bozza

--- src/it/mytutor/business/services/LessonInterface.java
+++ src/it/mytutor/business/services/LessonInterface.java
@@ -1,8 +1,10 @@
 package it.mytutor.business.services;
 
 import it.mytutor.domain.Lesson;
+import it.mytutor.domain.Planning;
 import it.mytutor.domain.Subject;
 import it.mytutor.domain.Teacher;
+import it.mytutor.domain.dao.exception.DatabaseException;
 
 import java.util.List;
 
@@ -11,9 +13,9 @@ public interface LessonInterface {
     List<Lesson> findAllLessonByTeacher(Teacher teacher);
     List<Lesson> findAllLessonBySubject(Subject subject);
 //    TODO da fare la logica del metodo findLessonByFilter sulla business
-    List<Lesson> findLessonByFilter(String macroMateria, String nome, String zona, String microMateria,
-                                    String giornoSettimana, String prezzo, String oraInizio, String oraFine);
+//    List<Lesson> findLessonByFilter(String macroMateria, String nome, String zona, String microMateria,
+//                                    String giornoSettimana, String prezzo, String oraInizio, String oraFine) throws DatabaseException;
     Lesson findLessonByID(Integer idLesson);
-    Lesson createLesson(Lesson lesson);
-    Lesson updateLessson(Lesson lesson);
+    Planning createLesson(Planning planning);
+    Planning updateLessson(Planning planning);
 }
