6b668efa5e7907dab7226ccb0b2c163d274f760c
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-13 09:05:23 +0100

    LezioneRest Bozza

--- src/it/mytutor/api/LezioniRest.java
+++ src/it/mytutor/api/LezioniRest.java
@@ -1,92 +1,167 @@
 package it.mytutor.api;
 
+import it.mytutor.business.exceptions.UserException;
 import it.mytutor.business.impl.LessonBusiness;
+import it.mytutor.business.impl.UserBusiness;
 import it.mytutor.business.services.LessonInterface;
+import it.mytutor.business.services.UserInterface;
 import it.mytutor.domain.Lesson;
+import it.mytutor.domain.Planning;
+import it.mytutor.domain.Student;
 import it.mytutor.domain.Teacher;
+import it.mytutor.domain.dao.exception.DatabaseException;
 
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.ArrayList;
 import java.util.List;
 
-@Path("auth/{SID}/lezioni")
+@Path("lezioni")
 public class LezioniRest {
 
     private LessonInterface lessonService = new LessonBusiness();
+    private UserInterface userService = new UserBusiness();
 
     @GET
+    @Path("teach")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    @PermitAll
-    public Response getLezioni(@PathParam("SID") String sid, @QueryParam("filtro") String filtro, @QueryParam("numero") Integer numero, @QueryParam("sotto") Integer sotto) {
-
-        List<Lesson> lessons = new ArrayList<Lesson>();
-        List<Lesson> lessons20 = new ArrayList<Lesson>();
-        Integer index = -1;
-        String oof = "";
-
-//      ############ SENZA FILTRO #############
-        if (filtro == null) {
-//          ############ TEACHER #############
-            if (true == true) {                                       //TODO Dal SID capire se l'utente è un Teacher
-                Teacher teacher = new Teacher();
-                lessons.addAll(lessonService.findAllLessonByTeacher(teacher));
-//          ############ STUDENT #############
-            } else if (false == false) {                              //TODO Dal SID capire se l'utente è uno Student
-                lessons.addAll(lessonService.findAllLesson());
-            }
-        }
-//      ############ CON FILTRO #############
-        else {
-//              ############ TEACHER #############
-            if (true == true) {                                       //Da SID capire se l'utente è un Teacher
-                //TODO da definire il filtro e se inserirlo per il Teacher
-                lessons.addAll(lessonService.findLessonByFilter(filtro));
-//              ############ STUDENT #############
-            } else if (false == false) {                              //Da SID capire se l'utente è uno Student
-                //TODO da definire il filtro
-                lessons.addAll(lessonService.findLessonByFilter(filtro));
-            }
-        }
-//      ############ PRIMA CHIAMATA #############
-        if (numero == null && sotto == null) {
-            if (lessons.size() > 20) {
-                lessons20 = lessons.subList(0, 20);
-            }else {
-                lessons20 = lessons.subList(0, lessons.size());
-            }
-        }
-//      ############ SUCCESSIVE CHIAMATE #############
-        else {
-            for (Lesson lesson : lessons) {
-                if (lesson.getIdLesson().equals(sotto)) {
-                    index = lessons.indexOf(lesson);
-                }
-            }
-            if (index < 0)
-                return Response.ok("nessun elemento").build();
-            if (lessons.size() > index + 1 + numero) {
-                lessons20 = lessons.subList(index + 1 , index + 1  + numero);
-            } else {
-                System.out.println("else");
-                if (index + 1 == lessons.size())
-                    return Response.ok("nessun elemento").build();
-                lessons20 = lessons.subList(index + 1 , lessons.size());
-            }
-        }
-        return Response.ok(lessons20).build();
+    @RolesAllowed({"TEACHER"})
+    public Response getLezioniTeachAll(SecurityContext sc) throws UserException {
+        String teacherEmail = sc.getUserPrincipal().getName();
+        Teacher teacher = (Teacher) userService.findUserByUsername(teacherEmail);
+        List<Lesson> lessons = new ArrayList<>(lessonService.findAllLessonByTeacher(teacher));
+        return Response.ok(lessons).build();
     }
+
+    @GET
+    @Path("stud")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({"STUDENT"})
+    public Response getLezioniStudAll(@QueryParam("macro-materia") String macroMateria,
+                                      @QueryParam("nome") String nome,
+                                      @QueryParam("zona") String zona,
+                                      @QueryParam("micro-materia") String microMateria,
+                                      @QueryParam("giorno-settimana") String giornoSettimana,
+                                      @QueryParam("prezzo") String prezzo,
+                                      @QueryParam("ora-inizio") String oraInizio,
+                                      @QueryParam("ora-fine") String oraFine) throws DatabaseException {
+
+        List<Lesson> lessons = new ArrayList<>(lessonService.findLessonByFilter(macroMateria, nome, zona,
+                microMateria, giornoSettimana, prezzo, oraInizio, oraFine));
+        return Response.ok(lessons).build();
+    }
+//    @GET
+//    @Path("teach")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @RolesAllowed({"TEACHER"})
+//    public Response getLezioniTeach(@QueryParam("filtro") String filtro, @QueryParam("numero") Integer numero, @QueryParam("sotto") Integer sotto) {
+//
+//        List<Lesson> lessons = new ArrayList<Lesson>();
+//        List<Lesson> lessons20 = new ArrayList<Lesson>();
+//        int index = -1;
+//        String oof = "";
+//
+////      ############ SENZA FILTRO #############
+//        if (filtro == null) {
+//                Teacher teacher = new Teacher();
+//                lessons.addAll(lessonService.findAllLessonByTeacher(teacher));
+//        }
+////      ############ CON FILTRO #############
+//        else {
+//                lessons.addAll(lessonService.findLessonByFilter(filtro));
+//        }
+////      ############ PRIMA CHIAMATA #############
+//        if (numero == null && sotto == null) {
+//            if (lessons.size() > 20) {
+//                lessons20 = lessons.subList(0, 20);
+//            }else {
+//                lessons20 = lessons.subList(0, lessons.size());
+//            }
+//        }
+////      ############ SUCCESSIVE CHIAMATE #############
+//        else {
+//            for (Lesson lesson : lessons) {
+//                if (lesson.getIdLesson().equals(sotto)) {
+//                    index = lessons.indexOf(lesson);
+//                }
+//            }
+//            if (index < 0)
+//                return Response.ok("nessun elemento").build();
+//            if (lessons.size() > index + 1 + numero) {
+//                lessons20 = lessons.subList(index + 1 , index + 1  + numero);
+//            } else {
+//                System.out.println("else");
+//                if (index + 1 == lessons.size())
+//                    return Response.ok("nessun elemento").build();
+//                lessons20 = lessons.subList(index + 1 , lessons.size());
+//            }
+//        }
+//        return Response.ok(lessons20).build();
+//    }
+
+//    @GET
+//    @Path("stud")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @RolesAllowed({"STUDENT"})
+//    public Response getLezioniStud( @QueryParam("filtro") String filtro, @QueryParam("numero") Integer numero, @QueryParam("sotto") Integer sotto) {
+//        List<Lesson> lessons = new ArrayList<Lesson>();
+//        List<Lesson> lessons20 = new ArrayList<Lesson>();
+//        int index = -1;
+//        String oof = "";
+//
+////      ############ SENZA FILTRO #############
+//        if (filtro == null) {
+//            lessons.addAll(lessonService.findAllLesson());
+//        }
+////      ############ CON FILTRO #############
+//        else {
+//            lessons.addAll(lessonService.findLessonByFilter(filtro));
+//        }
+////      ############ PRIMA CHIAMATA #############
+//        if (numero == null && sotto == null) {
+//            if (lessons.size() > 20) {
+//                lessons20 = lessons.subList(0, 20);
+//            }else {
+//                lessons20 = lessons.subList(0, lessons.size());
+//            }
+//        }
+////      ############ SUCCESSIVE CHIAMATE #############
+//        else {
+//            for (Lesson lesson : lessons) {
+//                if (lesson.getIdLesson().equals(sotto)) {
+//                    index = lessons.indexOf(lesson);
+//                }
+//            }
+//            if (index < 0)
+//                return Response.ok("nessun elemento").build();
+//            if (lessons.size() > index + 1 + numero) {
+//                lessons20 = lessons.subList(index + 1 , index + 1  + numero);
+//            } else {
+//                System.out.println("else");
+//                if (index + 1 == lessons.size())
+//                    return Response.ok("nessun elemento").build();
+//                lessons20 = lessons.subList(index + 1 , lessons.size());
+//            }
+//        }
+//        return Response.ok(lessons20).build();
+//    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @PermitAll
-    public Response creaLezione(Lesson lesson) {
-        Lesson lesson1 = lessonService.createLesson(lesson);
-        System.out.println(lesson1);
+    public Response creaLezione(Planning planning) {
+        System.out.println(planning);
+//        Planning planning1 = lessonService.createLesson(planning);
+//        System.out.println(planning1);
         return Response.ok().build();
 
     }
@@ -110,9 +185,8 @@ public class LezioniRest {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @PermitAll
-    public Response modificaLezione(Lesson lesson, @PathParam("LID") Integer lid) {
-
-        lessonService.updateLessson(lesson);
+    public Response modificaLezione(Planning plenning, @PathParam("LID") Integer lid) {
+        lessonService.updateLessson(plenning);
         return Response.ok().build();
     }
 
