4c58a0f811e145bcdc4f6bd5ff88c268d085b9b5
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-13 09:06:02 +0100

    MaterieRest Bozza

--- src/it/mytutor/api/MaterieRest.java
+++ src/it/mytutor/api/MaterieRest.java
@@ -1,35 +1,47 @@
 package it.mytutor.api;
+import it.mytutor.business.impl.SubjectBusiness;
+import it.mytutor.business.services.SubjectInterface;
+import it.mytutor.domain.Subject;
 
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import java.util.ArrayList;
+import java.util.List;
 
-@Path("auth/materie")
+@Path("materie")
 public class MaterieRest {
+
+    private SubjectInterface subjectService = new SubjectBusiness();
 
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.APPLICATION_JSON)
     @PermitAll
-    public String getMaterie(@QueryParam("filtro") String filtro){
-        return "<h1 style=\"" +
-                "color: red; "+
-                "margin: auto; " +
-                "width: fit-content; " +
-                "margin-top: 20%;\" " +
-                ">Componente Materie con @QueryParam(\"filtro\"):" + filtro + " (a default vale null)</h1>";
+    public Response getMaterie(SecurityContext ciao, @QueryParam("macro-materia") String macroMateria, @QueryParam("micro-materia") String microMateria){
+        List<Subject> subjects = new ArrayList<>();
+        // TODO Per riprendere l'email che è utile per tornare l'user tramite una chiamata al DB
+        // ciao.getUserPrincipal().getName()
+        if (macroMateria == null && microMateria == null){
+            subjects = subjectService.findAll();
+        } else if (macroMateria != null && microMateria == null){
+            subjects = subjectService.findAllByMacroSubject(macroMateria);
+        }else if (macroMateria == null){
+            subjects = subjectService.findAllByMicroSubject(microMateria);
+        }
+        return Response.ok(subjects).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.APPLICATION_JSON)
     @PermitAll
-    public String creaMaterie(){
-        return "<h1 style=\"" +
-                "color: red; "+
-                "margin: auto; " +
-                "width: fit-content; " +
-                "margin-top: 20%;\" " +
-                ">Crea Materie</h1>";
+    public Response creaMaterie(Subject subject){
+        System.out.println(subject);
+        subjectService.createSubject(subject);
+        System.out.println("nuova subject creata");
+        return Response.ok().build();
     }
 }
