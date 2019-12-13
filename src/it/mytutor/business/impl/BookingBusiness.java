a2e5be4e40743355a2f3bb52b5c87221aa61e7f1
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-13 14:26:05 +0100

    Bozza Booking Business per le funzioni di storico

--- src/it/mytutor/business/impl/BookingBusiness.java
+++ src/it/mytutor/business/impl/BookingBusiness.java
@@ -7,16 +7,15 @@ import it.mytutor.domain.Student;
 import it.mytutor.domain.Teacher;
 import it.mytutor.domain.dao.exception.DatabaseException;
 import it.mytutor.domain.dao.implement.BookingDao;
-import it.mytutor.domain.dao.implement.LessonDao;
 
 import java.sql.Date;
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import static it.mytutor.business.impl.test.TestBusinness.simulateFindAllBooking;
 
 public class BookingBusiness implements BookingInterface {
-    //TODO Costruttore Booking
 
     @Override
     public List<Booking> findAllBooking() {
@@ -27,7 +26,7 @@ public class BookingBusiness implements BookingInterface {
     public List<Booking> findBookingByFilter(String macroMateria, String nome, String zona, String microMateria,
                                            String giornoSettimana, String prezzo, String oraInizio, String oraFine) throws DatabaseException {
         BookingDao bookingDao = new BookingDao();
-        List<Booking> bookings = new ArrayList<>();
+        List<Booking> bookings;
         int macroMateriaRelevant  = 0;
         if (macroMateria != null && !macroMateria.isEmpty()){
             macroMateriaRelevant = 1;
@@ -110,9 +109,8 @@ public class BookingBusiness implements BookingInterface {
     }
 
     @Override
-    public List<Booking> findHistoricalBookingByStudent(Student student, String nomeLezione, String macroMateria,
-                                                        String microMateria, String idTeacher, String date,
-                                                        String rifiutata, String annullata, String eseguita) throws DatabaseException {
+    public List<Booking> findHistoricalBookingByStudent(Student student, String nomeLezione, String macroMateria, String microMateria,
+                                                        String idTeacher, String date, String rifiutata, String annullata, String eseguita) throws DatabaseException, ParseException {
         BookingDao bookingDao = new BookingDao();
         int nomeLezioneRevelant = 0;
         if (nomeLezione != null && !nomeLezione.isEmpty()){
@@ -126,21 +124,88 @@ public class BookingBusiness implements BookingInterface {
         if (microMateria != null && !microMateria.isEmpty()){
             microMateriaRelevant = 1;
         }
-        int idTeacherRevelant = 0;
-//        if ()
-//      TODO da finire con i revelant
-        List<Booking> listas = new ArrayList<>();
+        int idTeacherRelevant = 0;
+        int idTeacherAppo = 0;
+        if (idTeacher != null && !idTeacher.isEmpty()){
+            idTeacherAppo = Integer.parseInt(idTeacher);
+            idTeacherRelevant = 1;
+        }
+        int dateRelevant = 0;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateSql = null;
+        if (date != null && !date.isEmpty()){
+            java.util.Date dateUtil = sdf.parse(date);
+            dateSql = new Date(dateUtil.getTime());
+            dateRelevant = 1;
+        }
+        int rifiutataRelevant = 0;
+        if (rifiutata != null && !rifiutata.isEmpty()){
+            rifiutataRelevant = 1;
+        }
+        int annullataRelevant = 0;
+        if (annullata != null && !annullata.isEmpty()){
+            annullataRelevant = 1;
+        }
+        int eseguitaRelevant = 0;
+        if (eseguita != null && !eseguita.isEmpty()){
+            eseguitaRelevant = 1;
+        }
 
-        return listas;
+        return bookingDao.getHistoricalBokingsOfAStudent(student, nomeLezioneRevelant, nomeLezione, macroMateriaRelevant,
+                macroMateria, microMateriaRelevant, microMateria, idTeacherRelevant, idTeacherAppo, dateRelevant, dateSql,
+                rifiutataRelevant, rifiutata, annullataRelevant, annullata, eseguitaRelevant, eseguita);
     }
 
     @Override
-    public List<Booking> findHistoricalBookingByTeacher(Teacher teacher, String nomeLezione, String macroMateria,
-                                                        String microMateria, String idTeacher, String date,
-                                                        String rifiutata, String annullata, String eseguita) throws DatabaseException {
+    public List<Booking> findHistoricalBookingByTeacher(Teacher teacher, String nomeLezione, String macroMateria, String microMateria,
+                                                        String idStudent, String date, String rifiutata, String annullata, String eseguita) throws DatabaseException{
         BookingDao bookingDao = new BookingDao();
-        List<Booking> listas = new ArrayList<>();
+        int nomeLezioneRevelant = 0;
+        if (nomeLezione != null && !nomeLezione.isEmpty()){
+            nomeLezioneRevelant = 1;
+        }
+        int macroMateriaRelevant  = 0;
+        if (macroMateria != null && !macroMateria.isEmpty()){
+            macroMateriaRelevant = 1;
+        }
+        int microMateriaRelevant = 0;
+        if (microMateria != null && !microMateria.isEmpty()){
+            microMateriaRelevant = 1;
+        }
+        int idStudentRelevant = 0;
+        int idStudentAppo = 0;
+        if (idStudent != null && !idStudent.isEmpty()){
+            idStudentAppo = Integer.parseInt(idStudent);
+            idStudentRelevant = 1;
+        }
+        int dateRelevant = 0;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateSql = null;
+        if (date != null && !date.isEmpty()){
+            java.util.Date dateUtil;
+            try {
+                dateUtil = sdf.parse(date);
+                dateSql = new Date(dateUtil.getTime());
+                dateRelevant = 1;
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        int rifiutataRelevant = 0;
+        if (rifiutata != null && !rifiutata.isEmpty()){
+            rifiutataRelevant = 1;
+        }
+        int annullataRelevant = 0;
+        if (annullata != null && !annullata.isEmpty()){
+            annullataRelevant = 1;
+        }
+        int eseguitaRelevant = 0;
+        if (eseguita != null && !eseguita.isEmpty()){
+            eseguitaRelevant = 1;
+        }
 
-        return listas;
+        return bookingDao.getHistoricalBokingsOfATeacher(teacher, nomeLezioneRevelant, nomeLezione, macroMateriaRelevant,
+                macroMateria, microMateriaRelevant, microMateria, idStudentRelevant, idStudentAppo, dateRelevant, dateSql,
+                rifiutataRelevant, rifiutata, annullataRelevant, annullata, eseguitaRelevant, eseguita);
     }
 }
