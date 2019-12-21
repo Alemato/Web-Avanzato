630bf45359330f825da1d1bd69c59e89292b0c18
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-12-21 16:25:15 +0100

    Implememntazione di LoginRest

--- src/it/mytutor/api/LoginRest.java
+++ src/it/mytutor/api/LoginRest.java
@@ -1,48 +1,55 @@
 package it.mytutor.api;
 
+import it.mytutor.api.test.ApiWebApplicationException;
+import it.mytutor.api.test.UserCredentials;
+import it.mytutor.business.exceptions.UserException;
+import it.mytutor.business.impl.UserBusiness;
+import it.mytutor.business.security.AuthenticationTokenService;
+import it.mytutor.business.security.securityexception.AuthenticationException;
+import it.mytutor.domain.Student;
+import it.mytutor.domain.Teacher;
+import it.mytutor.domain.User;
+
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("auth")
 public class LoginRest {
-    @GET
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_HTML)
-    @PermitAll
-    public String getAuth(){
-        return "<h1 style=\"" +
-                "color: red; "+
-                "margin: auto; " +
-                "width: fit-content; " +
-                "margin-top: 20%;\" " +
-                ">Componente di login</h1>";
-    }
+    private UserBusiness userService = new UserBusiness();
+    private AuthenticationTokenService authenticationTokenService = new AuthenticationTokenService();
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.APPLICATION_JSON)
     @PermitAll
-    public String getAuthToken(){
-        return "<h1 style=\"" +
-                "color: red; "+
-                "margin: auto; " +
-                "width: fit-content; " +
-                "margin-top: 20%;\" " +
-                ">RILASCIA TOKEN </h1>";
-    }
-
-    @DELETE
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_HTML)
-    @PermitAll
-    public String deleteToken(){
-        System.out.println("Accedo al sid");
-        return "<h1 style=\"" +
-                "color: red; "+
-                "margin: auto; " +
-                "width: fit-content; " +
-                "margin-top: 20%;\" " +
-                ">ELIMINA TOKEN </h1>";
+    public Response getAuth(UserCredentials credentials){
+        try {
+            Object utente = userService.autentication(credentials.getUsername(), credentials.getPassword());
+            System.out.println(utente.toString());
+            String token = authenticationTokenService.generateToken(utente);
+            if (utente  instanceof Student){
+                Student student= (Student) utente;
+                return Response.ok(student).header("X-Auth", token).header("X-User-Type", "student").build();
+            }else if(utente instanceof Teacher){
+                Teacher teacher= (Teacher) utente;
+                return Response.ok(teacher).header("X-Auth", token).header("X-User-Type", "teacher").build();
+            }else if(utente instanceof User){
+                User admin = (User) utente;
+                return Response.ok(admin).header("X-Auth", token).header("X-User-Type", "admin").build();
+            } else throw new ApiWebApplicationException("bad credentials");
+        } catch (UserException e) {
+            e.printStackTrace();
+            if (e.getMessage().equals("AUTENTICAZIONE NON VALIDA")){
+                throw new ApiWebApplicationException("credenziali non valide", 401);
+            } else throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
+        } catch (AuthenticationException authenticationException) {
+            authenticationException.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
+        }
     }
 }
