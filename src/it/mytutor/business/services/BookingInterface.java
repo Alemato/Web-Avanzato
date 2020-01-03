37067eb2cd6caf6eecf8f03632800ce5b3284be3
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-03 19:57:58 +0100

    Debug booking interface

--- src/it/mytutor/business/services/BookingInterface.java
+++ src/it/mytutor/business/services/BookingInterface.java
@@ -23,7 +23,7 @@ public interface BookingInterface {
     List<Booking> findAllBookingByStudnet(Student student) throws DatabaseException, PlanningBusinessException, BookingBusinessException, UserException;
     List<Booking> findAllBookingByTeacher(Teacher teacher) throws DatabaseException, PlanningBusinessException, BookingBusinessException, UserException;
     List<Booking> findAllBookingByLesson(Lesson lesson);
-    void crateBooking(Booking booking) throws PlanningBusinessException;
+    void crateBookings(List<Booking> bookings) throws PlanningBusinessException;
     void updateBooking(Booking booking, Integer lessonState) throws BookingBusinessException;
     List<Booking> findHistoricalBookingByStudent(Student student) throws DatabaseException, ParseException, PlanningBusinessException, BookingBusinessException, UserException;
     List<Booking> findHistoricalBookingByTeacher(Teacher teacher) throws Exception;
