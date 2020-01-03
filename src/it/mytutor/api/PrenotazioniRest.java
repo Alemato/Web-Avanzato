1b4329eca480e1bbc149224522c3eabc137fb418
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-03 14:52:47 +0100

    bozza quasi definitiva PrenotazioniRest

--- src/it/mytutor/api/PrenotazioniRest.java
+++ src/it/mytutor/api/PrenotazioniRest.java
@@ -1,47 +1,83 @@
 package it.mytutor.api;
 
-import io.jsonwebtoken.lang.Strings;
+import it.mytutor.api.test.ApiWebApplicationException;
+import it.mytutor.business.exceptions.BookingBusinessException;
+import it.mytutor.business.exceptions.PlanningBusinessException;
+import it.mytutor.business.exceptions.UserException;
 import it.mytutor.business.impl.BookingBusiness;
-import it.mytutor.business.impl.LessonBusiness;
+import it.mytutor.business.impl.UserBusiness;
 import it.mytutor.business.services.BookingInterface;
-import it.mytutor.business.services.LessonInterface;
+import it.mytutor.business.services.UserInterface;
 import it.mytutor.domain.Booking;
 import it.mytutor.domain.Student;
-import it.mytutor.domain.Teacher;
+import it.mytutor.domain.dao.exception.DatabaseException;
 
-import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
+import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
+import javax.ws.rs.core.SecurityContext;
 
-@Path("auth/lezioni/{LID}/prenotazioni")
+@Path("lezioni/prenotazioni")
 public class PrenotazioniRest {
 
     private BookingInterface bookingService = new BookingBusiness();
-    private String lid;
+    private UserInterface userService = new UserBusiness();
 
-    public PrenotazioniRest(@PathParam("LID") String lid){
-        this.lid = lid;
-    }
-
+    /**
+     * Creazione della prenotazione da parte dello studente
+     *
+     * @param booking oggetto prenotazione ricevuto dal client
+     * @param context ContainerRequestContext da cui prendere l'email dell'utente
+     * @return Response Status ACCEPTED
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response creaPrenotazione(Booking booking){
-        bookingService.crateBooking(booking);
-        return Response.ok().build();
+    @RolesAllowed({"STUDENT"})
+    public Response creaPrenotazione(Booking booking, ContainerRequestContext context){
+
+        SecurityContext securityContext = context.getSecurityContext();
+        String teacherEmail = securityContext.getUserPrincipal().getName();
+        Student student;
+        try {
+            student = (Student) userService.findUserByUsername(teacherEmail);
+        } catch (UserException | DatabaseException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
+        booking.setStudent(student);
+        try {
+            bookingService.crateBooking(booking);
+        } catch (PlanningBusinessException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
+        return Response.ok(Response.Status.ACCEPTED).entity("prenotazione creata").build();
     }
 
+    /**
+     * Rest per l'aggiornamento dello stato della lezione
+     * concesso ad entrambe le tipologie di utenti
+     *
+     *
+     * @param pid id booking
+     * @param lessonState stato della lezione
+     * @return Response Status ACCEPTED
+     */
     @Path("{PID}")
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response rispostaTheacher(@PathParam("PID") Integer pid, @QueryParam("status") Integer lessonState){
-        bookingService.updateBooking(bookingService.findBookingById(pid), lessonState);
-        return Response.ok().build();
+    @RolesAllowed({"TEACHER", "STUDENT"})
+    public Response rispostaTheacher(@PathParam("PID") Integer pid, @QueryParam("lesson-state") Integer lessonState){
+        try {
+            bookingService.updateBooking(bookingService.findBookingById(pid), lessonState);
+        } catch (BookingBusinessException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
+        return Response.ok(Response.Status.ACCEPTED).entity("prenotazione aggiornata").build();
     }
 }
