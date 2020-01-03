7a95ef62f6ccc735679ee6351114909e4b398ab3
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-03 14:52:07 +0100

    bozza quasi definitiva PlanningRest

--- src/it/mytutor/api/PlanningRest.java
+++ src/it/mytutor/api/PlanningRest.java
@@ -1,14 +1,21 @@
 package it.mytutor.api;
 
+import it.mytutor.api.test.ApiWebApplicationException;
+import it.mytutor.business.exceptions.*;
 import it.mytutor.business.impl.PlanningBusiness;
+import it.mytutor.business.impl.UserBusiness;
 import it.mytutor.business.services.PlanningInterface;
+import it.mytutor.business.services.UserInterface;
 import it.mytutor.domain.Planning;
+import it.mytutor.domain.Teacher;
 import it.mytutor.domain.dao.exception.DatabaseException;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
+import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.List;
 
 
@@ -16,15 +23,51 @@ import java.util.List;
 public class PlanningRest {
 
     private PlanningInterface planningService = new PlanningBusiness();
+    private UserInterface userService = new UserBusiness();
 
+    /**
+     * Rest Creazione del planning concesa solamente al professore
+     *
+     * @param plannings Lista di Pianificazioni della prenotazione della lezione
+     * @return Response Status ACCEPTED
+     */
     @Path("crea")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     @RolesAllowed({"TEACHER"})
-    public Response CreaPlanning(List<Planning> plannings) {
-        planningService.CreaPlanning(plannings);
-        return Response.ok().build();
+    public Response creaPlanning(List<Planning> plannings, ContainerRequestContext context) {
+
+        SecurityContext securityContext = context.getSecurityContext();
+        String teacherEmail = securityContext.getUserPrincipal().getName();
+        Teacher teacher;
+        try {
+            teacher = (Teacher) userService.findUserByUsername(teacherEmail);
+        } catch (UserException | DatabaseException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
+        try {
+            planningService.creaPlanning(plannings, teacher);
+        } catch (PlanningBusinessException | SubjectBusinessException | LessonBusinessException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
+        return Response.ok(Response.Status.ACCEPTED).entity("Lezione pianificata").build();
+    }
+
+    @Path("modifica")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed({"TEACHER"})
+    public Response updateLesson(List<Planning> plannings) {
+        try {
+            planningService.updatePlanning(plannings);
+        } catch (PlanningBusinessException e) {
+            e.printStackTrace();
+        }
+        return Response.ok(Response.Status.ACCEPTED).entity("Lezione modificata").build();
     }
 
     @Path("research")
@@ -39,12 +82,17 @@ public class PlanningRest {
                                            @QueryParam("giorno-settimana") String giornoSettimana,
                                            @QueryParam("prezzo") String prezzo,
                                            @QueryParam("ora-inizio") String oraInizio,
-                                           @QueryParam("ora-fine") String oraFine) throws DatabaseException {
+                                           @QueryParam("ora-fine") String oraFine) {
         List<Planning> plannings;
-
-        plannings = planningService.FindPlanningByFilter(macroMateria, nome, zona, microMateria, giornoSettimana, prezzo, oraInizio, oraFine);
-
+        try {
+            plannings = planningService.FindPlanningByFilter(macroMateria, nome, zona, microMateria, giornoSettimana, prezzo, oraInizio, oraFine);
+        } catch (DatabaseException | PlanningBusinessException | BookingBusinessException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
+        }
         return  Response.ok(plannings).build();
     }
+
+    //TODO rest per liste dei campi in storico lezione per professore e per studente per Booking LessonState 2,3,4
 
 }
