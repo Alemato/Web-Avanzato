19910732d9a637f696948df4035b754f4861cb78
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-05 20:11:48 +0100

    creates interfaces

--- src/it/mytutor/business/services/CreatesInterface.java
+++ src/it/mytutor/business/services/CreatesInterface.java
@@ -3,6 +3,6 @@ package it.mytutor.business.services;
 import it.mytutor.domain.Creates;
 
 public interface CreatesInterface {
-    Creates createCreates(Creates creates);
+    void createCreates(Creates creates);
     Creates getCreates(int id);
 }
