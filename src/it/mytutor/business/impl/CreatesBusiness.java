9ccc5ad87071b72bec1021d6bfe12e1f963dcb49
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-05 20:11:30 +0100

    Bozza Chat e creates

--- src/it/mytutor/business/impl/CreatesBusiness.java
+++ src/it/mytutor/business/impl/CreatesBusiness.java
@@ -2,14 +2,27 @@ package it.mytutor.business.impl;
 
 import it.mytutor.business.services.CreatesInterface;
 import it.mytutor.domain.Creates;
+import it.mytutor.domain.dao.exception.DatabaseException;
+import it.mytutor.domain.dao.implement.CreatesDao;
+import it.mytutor.domain.dao.interfaces.CreatesDaoInterface;
 
 public class CreatesBusiness implements CreatesInterface {
-    //TODO Costruttore Creates
 
     @Override
-    public Creates createCreates(Creates creates){ return creates; }
+    public void createCreates(Creates creates) {
+        CreatesDaoInterface createsDao = new CreatesDao();
+        try {
+            createsDao.createCreates(creates);
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
     @Override
-    public Creates getCreates(int id){
-        Creates creates=null;
-        return creates;}
+    public Creates getCreates(int id) {
+        Creates creates = null;
+        return creates;
+    }
 }
