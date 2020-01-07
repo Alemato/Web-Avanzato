3d28d4bfe9a6a751843204aee2d8bb666c7611ed
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-07 11:08:21 +0100

    Debug e modifica materie rest

--- src/it/mytutor/api/MaterieRest.java
+++ src/it/mytutor/api/MaterieRest.java
@@ -2,9 +2,16 @@ package it.mytutor.api;
 
 import it.mytutor.api.test.ApiWebApplicationException;
 import it.mytutor.business.exceptions.SubjectBusinessException;
+import it.mytutor.business.exceptions.UserException;
 import it.mytutor.business.impl.SubjectBusiness;
+import it.mytutor.business.impl.UserBusiness;
 import it.mytutor.business.services.SubjectInterface;
+import it.mytutor.business.services.UserInterface;
+import it.mytutor.domain.Student;
 import it.mytutor.domain.Subject;
+import it.mytutor.domain.Teacher;
+import it.mytutor.domain.User;
+import it.mytutor.domain.dao.exception.DatabaseException;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
@@ -12,6 +19,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("subject")
@@ -21,11 +29,12 @@ public class MaterieRest {
 
 
     private SubjectInterface subjectService = new SubjectBusiness();
+    private UserInterface userService = new UserBusiness();
 
-    /**
+   /* *//**
      * rest per i menu di select di inserimento lezione e di ricerca lezione
      * @return lista di subject contenenti materie e micromaterie usate per i menu select
-     */
+     *//*
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -39,7 +48,7 @@ public class MaterieRest {
             throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
         }
         return Response.ok(subjects).build();
-    }
+    }*/
 
 
     /** rest per i menu di select di inserimento lezione e di ricerca lezione e per lo storico
@@ -52,12 +61,24 @@ public class MaterieRest {
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed({"STUDENT", "TEACHER"})
     public Response getMaterie(@QueryParam("history") String storico) {
-        List<Subject> subjects;
+        List<Subject> subjects = new ArrayList<>();
         if (storico != null && !storico.isEmpty()) {
             try {
                 String email = securityContext.getUserPrincipal().getName();
-                subjects = subjectService.findAllStorico(email);
+                User user = (User) userService.findUserByUsername(email);
+                if (user.getRoles() == 1) {
+                    subjects = subjectService.findAllStoricoStudent(email);
+                } else if (user.getRoles() == 2) {
+                    subjects = subjectService.findAllStoricoTeacher(email);
+                }
+
             } catch (SubjectBusinessException e) {
+                e.printStackTrace();
+                throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+            } catch (DatabaseException e) {
+                e.printStackTrace();
+                throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+            } catch (UserException e) {
                 e.printStackTrace();
                 throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
             }
@@ -72,11 +93,11 @@ public class MaterieRest {
         return Response.ok(subjects).build();
     }
 
-    /**
+   /* *//**
      * Rest solo per storico
      *
      * @return lista di subject contenenti materie e micromaterie usate per i menu select
-     */
+     *//*
     @Path("history")
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
@@ -92,7 +113,7 @@ public class MaterieRest {
             throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
         }
         return Response.ok(subjects).build();
-    }
+    }*/
 
     /*@POST
     @Consumes(MediaType.APPLICATION_JSON)
