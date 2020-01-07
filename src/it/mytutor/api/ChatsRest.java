45fdd0517ef0585dcd6c2e24103a59038fbcb1c9
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-08 00:55:17 +0100

    bozza chatrest

--- src/it/mytutor/api/ChatsRest.java
+++ src/it/mytutor/api/ChatsRest.java
@@ -1,24 +1,29 @@
 package it.mytutor.api;
 
 import it.mytutor.api.test.ApiWebApplicationException;
+import it.mytutor.business.exceptions.ChatBusinessException;
+import it.mytutor.business.exceptions.UserException;
 import it.mytutor.business.impl.ChatBusiness;
 import it.mytutor.business.impl.CreatesBusiness;
 import it.mytutor.business.impl.MessageBusiness;
+import it.mytutor.business.impl.UserBusiness;
 import it.mytutor.business.services.ChatInterface;
 import it.mytutor.business.services.CreatesInterface;
 import it.mytutor.business.services.MessageInterface;
+import it.mytutor.business.services.UserInterface;
+import it.mytutor.domain.Chat;
 import it.mytutor.domain.Creates;
 import it.mytutor.domain.Message;
+import it.mytutor.domain.User;
 import it.mytutor.domain.dao.exception.DatabaseException;
 
-import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
-import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -30,6 +35,7 @@ public class ChatsRest {
     private ChatInterface chatService = new ChatBusiness();
     private MessageInterface messageService = new MessageBusiness();
     private CreatesInterface createsService = new CreatesBusiness();
+    private UserInterface userService = new UserBusiness();
 
     /**
      * rest per ricevere la lista di tutte le chat che saranno salvate nello storage
@@ -41,24 +47,66 @@ public class ChatsRest {
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed({"STUDENT", "TEACHER"})
     public Response getAllChat(){
+        User user;
+        String email = securityContext.getUserPrincipal().getName();
+        try {
+            user = (User) userService.findUserByUsername(email);
+        } catch (UserException | DatabaseException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
+        List<Object> list = new ArrayList<Object>();
+        List<Creates> creates = new ArrayList<>();
+        List<Message> messages = new ArrayList<>();
+        try {
+            messages = chatService.findAllChatByUser(email);
+        }catch (DatabaseException e){
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
+        }
+
+        try {
+            creates = createsService.getCreatesByIdUser(user.getIdUser());
+        } catch (ChatBusinessException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
+        }
+
+        list.add(messages);
+        list.add(creates);
+
+        return Response.ok(list).build();
+    }
+
+    /**
+     * rest per ricevere la lista di tutte le chat che saranno salvate nello storage
+     * Con un solo l'ULTIMO MESSAGGIO ricevuto o inviato.
+     * @return lista di ultimi messaggi per ogni chat
+     */
+    @Path("count")
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({"STUDENT", "TEACHER"})
+    public Response getChatCount(){
         String username = securityContext.getUserPrincipal().getName();
         List<Message> messageList;
         try {
             messageList = chatService.findAllChatByUser(username);
-            return Response.ok(messageList).build();
+            return Response.ok(messageList.size()).build();
         }catch (DatabaseException e){
             e.printStackTrace();
             throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
         }
     }
 
-    /**
+/*    *//**
      * Query rest chiamata per ricevere la lista di tutte le chat che
      * saranno salvate nello storage
      * gli passiamo 0 per tutte le chat sue
      * gli passiamo id del ultima per vedere le sole ultime chat
      * @return lista di primi messaggi di ogni chat rappresentante la lista delle chat
-     */
+     *//*
     @Path("byid")
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
@@ -72,25 +120,76 @@ public class ChatsRest {
                 messageList = chatService.findAllChatByUserByQuery(username);
                 return Response.ok(messageList).build();
             } else {
-               System.out.println("altro");
-               return Response.ok().build();
+                System.out.println("altro");
+
+
+                return Response.ok().build();
             }
         } catch (DatabaseException e) {
             e.printStackTrace();
             throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
         }
-    }
-
+    }*/
 
     @Path("post")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed({"STUDENT", "TEACHER"})
-    public Response createChats(@QueryParam("idAddressee") Integer addressee, @QueryParam("ChatName") String chatName) {
+    public Response createChat(@QueryParam("id-addressee") Integer addressee, @QueryParam("chat-name") String chatName) {
+        User user;
+        String email = securityContext.getUserPrincipal().getName();
+        try {
+            user = (User) userService.findUserByUsername(email);
+        } catch (UserException | DatabaseException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
 
+        if (user.getRoles().equals(1)) {
+            try {
+                createsService.createCreates(addressee, chatName, user.getIdUser());
+            } catch (ChatBusinessException e) {
+                e.printStackTrace();
+                throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+            }
+        } else {
+            try {
+                createsService.createCreates(user.getIdUser(), chatName, addressee);
+            } catch (ChatBusinessException e) {
+                e.printStackTrace();
+                throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+            }
+        }
         return Response.ok().build();
     }
+
+/*    @Path("creates")
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({"STUDENT", "TEACHER"})
+    public Response getCreates() {
+
+        User user;
+        String email = securityContext.getUserPrincipal().getName();
+        try {
+            user = (User) userService.findUserByUsername(email);
+        } catch (UserException | DatabaseException e) {
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
+        }
+
+        try {
+            creates = createsService.getCreatesbyId(user.getIdUser())
+
+        }catch (DatabaseException e){
+            e.printStackTrace();
+            throw new ApiWebApplicationException("Errore interno al server: "+ e.getMessage());
+        }
+        return Response.ok(creates).build();
+    }*/
+
 
 
 }
