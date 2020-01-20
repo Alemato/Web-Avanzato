c66b420e337eeb425555f936cc49ca3a1643e6fa
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-20 13:30:36 +0100

    Lesson Deserializer add teacher

--- src/it/mytutor/domain/jackson/LessonDeserializer.java
+++ src/it/mytutor/domain/jackson/LessonDeserializer.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import it.mytutor.domain.Lesson;
 import it.mytutor.domain.Subject;
+import it.mytutor.domain.Teacher;
 
 import java.io.IOException;
 import java.sql.Date;
@@ -43,6 +44,30 @@ public class LessonDeserializer extends StdDeserializer<Lesson> {
         subject.setMacroSubject(subjectNode.get("macroSubject").asText());
         subject.setMicroSubject(subjectNode.get("microSubject").asText());
         lesson.setSubject(subject);
+
+        JsonNode teacherNode = node.get("teacher");
+        Teacher teacher = new Teacher();
+        if (teacherNode.get("idTeacher") != null) {
+            teacher.setIdTeacher(teacherNode.get("idTeacher").asInt());
+        }
+        teacher.setPostCode(teacherNode.get("postCode").asInt());
+        teacher.setCity(teacherNode.get("city").asText());
+        teacher.setRegion(teacherNode.get("region").asText());
+        teacher.setStreet(teacherNode.get("street").asText());
+        teacher.setStreetNumber(teacherNode.get("streetNumber").asText());
+        teacher.setByography(teacherNode.get("byography").asText());
+        teacher.setIdUser(teacherNode.get("idUser").asInt());
+        teacher.setEmail(teacherNode.get("email").asText());
+        teacher.setPassword(teacherNode.get("password").asText());
+        teacher.setName(teacherNode.get("name").asText());
+        teacher.setSurname(teacherNode.get("surname").asText());
+        Date bDate = new Date(teacherNode.get("birthday").asLong());
+        teacher.setBirthday(bDate);
+        teacher.setLanguage(Boolean.getBoolean(teacherNode.get("language").asText()));
+        if(teacherNode.get("image").asText().equals("null") || teacherNode.get("image").asText().equals("")){
+            teacher.setImage(null);
+        } else teacher.setImage(teacherNode.get("image").asText());
+        lesson.setTeacher(teacher);
 
         return lesson;
     }
