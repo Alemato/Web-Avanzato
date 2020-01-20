5439dd56a07888c93133acf9ffeb35fe415f7055
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-20 13:31:19 +0100

    Message Deserializer debug idChat e user

--- src/it/mytutor/domain/jackson/MessageDeserializer.java
+++ src/it/mytutor/domain/jackson/MessageDeserializer.java
@@ -5,16 +5,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import it.mytutor.domain.Chat;
 import it.mytutor.domain.Message;
 import it.mytutor.domain.User;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class MessageDeserializer extends StdDeserializer<Message> {
     public MessageDeserializer() {
@@ -37,7 +34,9 @@ public class MessageDeserializer extends StdDeserializer<Message> {
 
         Chat chat = new Chat();
         JsonNode chatNode = node.get("chat");
-        chat.setIdChat(chatNode.get("idChat").asInt());
+        if (chatNode.get("idChat") != null) {
+            chat.setIdChat(chatNode.get("idChat").asInt());
+        }
         chat.setName(chatNode.get("chatName").asText());
         message.setChat(chat);
 
@@ -45,9 +44,15 @@ public class MessageDeserializer extends StdDeserializer<Message> {
         JsonNode userNode = node.get("user");
         user.setIdUser(userNode.findPath("idUser").asInt());
         user.setEmail(userNode.findPath("email").asText());
-//        user.setRoles(userNode.findPath("roles").asInt());
-//        user.setName(userNode.findPath("name").asText());
-//        user.setSurname(userNode.findPath("surname").asText());
+        user.setRoles(userNode.findPath("roles").asInt());
+        user.setName(userNode.findPath("name").asText());
+        user.setSurname(userNode.findPath("surname").asText());
+        Date bDate = new Date(userNode.get("birthday").asLong());
+        user.setBirthday(bDate);
+        user.setLanguage(Boolean.getBoolean(userNode.get("language").asText()));
+        if(userNode.get("image").asText().equals("null") || userNode.get("image").asText().equals("")){
+            user.setImage(null);
+        } else user.setImage(userNode.get("image").asText());
         message.setUser(user);
 
         return message;
