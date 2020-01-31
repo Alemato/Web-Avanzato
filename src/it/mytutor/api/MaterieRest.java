23f01fc0959bea53d63d0d31addc7a4808814218
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-31 22:40:13 +0100

    debug materie rest

--- src/it/mytutor/api/MaterieRest.java
+++ src/it/mytutor/api/MaterieRest.java
@@ -7,9 +7,7 @@ import it.mytutor.business.impl.SubjectBusiness;
 import it.mytutor.business.impl.UserBusiness;
 import it.mytutor.business.services.SubjectInterface;
 import it.mytutor.business.services.UserInterface;
-import it.mytutor.domain.Student;
 import it.mytutor.domain.Subject;
-import it.mytutor.domain.Teacher;
 import it.mytutor.domain.User;
 import it.mytutor.domain.dao.exception.DatabaseException;
 
@@ -31,27 +29,9 @@ public class MaterieRest {
     private SubjectInterface subjectService = new SubjectBusiness();
     private UserInterface userService = new UserBusiness();
 
-   /* *//**
-     * rest per i menu di select di inserimento lezione e di ricerca lezione
-     * @return lista di subject contenenti materie e micromaterie usate per i menu select
-     *//*
-    @GET
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed({"STUDENT", "TEACHER"})
-    public Response getMaterie() {
-        List<Subject> subjects;
-        try {
-            subjects = subjectService.findAll();
-        } catch (SubjectBusinessException e) {
-            e.printStackTrace();
-            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
-        }
-        return Response.ok(subjects).build();
-    }*/
-
-
-    /** rest per i menu di select di inserimento lezione e di ricerca lezione e per lo storico
+    /**
+     * rest per i menu di select di inserimento lezione e di ricerca lezione e per lo storico
+     *
      * @param storico parametro che identifica il fatto che si fa la richiesta dalla pagina dello storico
      * @return lista di subject contenenti materie e micromaterie usate per i menu select
      */
@@ -71,13 +51,7 @@ public class MaterieRest {
                     subjects = subjectService.findAllStoricoTeacher(email);
                 }
 
-            } catch (SubjectBusinessException e) {
-                e.printStackTrace();
-                throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
-            } catch (DatabaseException e) {
-                e.printStackTrace();
-                throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
-            } catch (UserException e) {
+            } catch (DatabaseException | UserException | SubjectBusinessException e) {
                 e.printStackTrace();
                 throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
             }
@@ -91,35 +65,4 @@ public class MaterieRest {
         }
         return Response.ok(subjects).build();
     }
-
-   /* *//**
-     * Rest solo per storico
-     *
-     * @return lista di subject contenenti materie e micromaterie usate per i menu select
-     *//*
-    @Path("history")
-    @GET
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed({"STUDENT", "TEACHER"})
-    public Response getMaterieStoriche() {
-        List<Subject> subjects;
-        String email = securityContext.getUserPrincipal().getName();
-        try {
-            subjects = subjectService.findAllStorico(email);
-        } catch (SubjectBusinessException e) {
-            e.printStackTrace();
-            throw new ApiWebApplicationException("Errore interno al server: " + e.getMessage());
-        }
-        return Response.ok(subjects).build();
-    }*/
-
-    /*@POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @PermitAll
-    public Response creaMaterie(Subject subject){
-        subjectService.createSubject(subject);
-        return Response.ok().build();
-    }*/
 }
